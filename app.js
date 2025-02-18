2580be48bc42d423da6e4de3b6e3193245f31657
RomanOlsen <romanjolsen@gmail.com>
2025-02-18 15:55:14 -0700

    array start

--- app.js
+++ app.js
@@ -4,9 +4,49 @@ let total = 0
 
 function addToCart(newOrderCost) {
   total = total + newOrderCost
-  // console.log("clicked!") 
   console.log(total)
+
+  document.getElementById("totalScreen")
+  totalScreen.innerText = total
 }
 
-// total = total + 1 // messing around
-// console.log(total) 
+let iceCream = [
+  // Regular icecream (cheap)
+  {
+    flavor: 'Chocolate',
+    isRegular: true,
+    quantity: 0,
+    price: 4
+  },
+  {
+    flavor: 'Vanilla',
+    isRegular: true,
+    quantity: 0,
+    price: 4
+  },
+  {
+    flavor: 'Mint Chocolate',
+    isRegular: true,
+    quantity: 0,
+    price: 4
+  },
+  //DAIRY FREE below
+  {
+    flavor: 'Chocolate',
+    isRegular: false,
+    quantity: 0,
+    price: 8
+  },
+  {
+    flavor: 'Vanilla',
+    isRegular: false,
+    quantity: 0,
+    price: 8
+  },
+  {
+    flavor: 'Mint Chocolate',
+    isRegular: false,
+    quantity: 0,
+    price: 8
+  },
+]
