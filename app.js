1ccdf179883452df7cb949eddd89c632cd7c2fd7
RomanOlsen <romanjolsen@gmail.com>
2025-02-18 15:25:26 -0700

    place order button

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log("hi")
 
-function addToCart() {
-  console.log("clicked!")
+let total = 0
+
+function addToCart(newOrderCost) {
+  total = total + newOrderCost
+  // console.log("clicked!") 
+  console.log(total)
 }
+
+// total = total + 1 // messing around
+// console.log(total) 
