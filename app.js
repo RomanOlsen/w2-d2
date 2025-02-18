c9f872b556191f3a45b97f3730e585dda3e14485
RomanOlsen <romanjolsen@gmail.com>
2025-02-18 16:23:39 -0700

    just another commit

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ console.log("hi")
 
 let total = 0
 
-function addToCart(newOrderCost) {
-  total = total + newOrderCost
-  console.log(total)
+//  function addToCart(newOrderCost) {
+//   total = total + newOrderCost
+//   console.log(total)
 
-  document.getElementById("totalScreen")
-  totalScreen.innerText = total
+//   document.getElementById("totalScreen")
+//   totalScreen.innerText = total
+// }
+
+function addToCart(arrayNumber) {
+  qty = iceCream[arrayNumber].quantity
+  iceCream[arrayNumber].quantity++
+  document.getElementById('qtyText')
+  qtyText.innerText = qty
 }
 
 let iceCream = [
-  // Regular icecream (cheap)
+  // Regular icecream (cheap) | ARRAY STARTS AT 0
   {
     flavor: 'Chocolate',
     isRegular: true,
